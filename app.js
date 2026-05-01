56b0f1153e0c1ecae3dc6790b3446c16a3c32f09
Krishna Srivastava <krishnasrivastava627@gmail.com>
2026-05-01 14:54:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret:'secret-key',
+    resave: false,
+}));
+app.get('/',(req,res)=>{
+    res.send("welcome");
+});
+app.post('/login',(req,res)=>{
+    const{username,role}=req.body;
+    req.session.username=username;
+    req.session.role=role;
+    res.cookie('role',role);
+    res.send('login succesfully');
+})
+app.get('/dashboard',(req,res)=>{
+    if(!req.session.username){
+        res.send('login first');
+    }
+    res.send(Welcome ${req.session.username},you are logged in as ${req.session.role});
+})
+app.get('/admin',(req,res)=>{
+    if(!req.session.username){
+        res.send('Please login');
+    }
+    if(req.session.role!=='admin'){
+        res.send('Access Denied');
+    }
+    res.send('Welcome Admin');
+});
+app.get('/logout',(req, res)=>{
+    req.session.destroy((err)=>{
+        if(err){
+           res.send('Error');
+        }
+        res.clearCookie('role');
+        res.send('Logout successful');
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('Server running 3000');
+});
